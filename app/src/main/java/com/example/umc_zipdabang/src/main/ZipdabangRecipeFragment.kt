d88e25cfdc5eb47b2e83cd9ffcc184b0f67935f4
Kim Gimun <kmkim7575@gmail.com>
2023-01-13 23:22:29 +0900

    [FEAT] 집다방 레시피 Fragment 추가

---
+++ app/src/main/java/com/example/umc_zipdabang/src/main/ZipdabangRecipeFragment.kt
@@ -0,0 +1,20 @@
+package com.example.umc_zipdabang.src.main
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.umc_zipdabang.databinding.FragmentZipdabangRecipeBinding
+
+class ZipdabangRecipeFragment: Fragment() {
+    private lateinit var viewBinding: FragmentZipdabangRecipeBinding
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        viewBinding = FragmentZipdabangRecipeBinding.inflate(inflater, container, false)
+        return viewBinding.root
+    }
+}
