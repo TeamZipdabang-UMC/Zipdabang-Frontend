3199bcccac78c96420657a75ca91732aa66d7b03
Kim Gimun <kmkim7575@gmail.com>
2023-01-13 22:36:43 +0900

    [FEAT] 초기 브랜치 생성

--- app/src/main/java/com/example/umc_zipdabang/src/main/MainActivity.kt
+++ app/src/main/java/com/example/umc_zipdabang/src/main/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.umc_zipdabang
+package com.example.umc_zipdabang.src.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.umc_zipdabang.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
