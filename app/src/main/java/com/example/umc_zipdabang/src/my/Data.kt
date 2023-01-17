5359ca5bf948b5f545fe2587a9a60e1b8c6c1931
jessy <yaeunjess@gmail.com>
2023-01-18 00:16:56 +0900

    그냥 커밋함

---
+++ app/src/main/java/com/example/umc_zipdabang/src/my/Data.kt
@@ -0,0 +1,3 @@
+package com.example.umc_zipdabang.src.my
+
+data class Data (val tital :String, val img : String)
---
+++ app/src/main/java/com/example/umc_zipdabang/src/my/MyFragment.kt
@@ -0,0 +1,5 @@
+package com.example.umc_zipdabang.src.my
+
+class MyFragment {
+
+}
