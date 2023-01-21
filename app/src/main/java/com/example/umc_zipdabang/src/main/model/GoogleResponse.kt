3c397151fc882eef425dfbb7455c3899cd631781
Kim Gimun <kmkim7575@gmail.com>
2023-01-21 18:13:22 +0900

    [FEAT] 소셜로그인 가져오는 정보 이메일 추가

--- app/src/main/java/com/example/umc_zipdabang/src/main/model/GoogleResponse.kt
+++ app/src/main/java/com/example/umc_zipdabang/src/main/model/GoogleResponse.kt
@@ -4,5 +4,6 @@ import com.google.gson.annotations.SerializedName
 
 data class GoogleResponse(
     @SerializedName ("status") val status: String,
+    @SerializedName ("email") val email: String,
     @SerializedName ("token") val token: String
 )
