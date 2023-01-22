ecc081f01cd528562d9590e834394da316c1a3e7
Kim Gimun <kmkim7575@gmail.com>
2023-01-23 00:14:09 +0900

    [FEAT] Room Database 추가

---
+++ app/src/main/java/com/example/umc_zipdabang/src/main/roomDb/CommentDao.kt
@@ -0,0 +1,26 @@
+package com.example.umc_zipdabang.src.main.roomDb
+
+import androidx.room.*
+
+@Dao
+interface CommentDao {
+    // 사용한다. 커멘트 하나를 넣는 작업
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun addComment(commentDbClass: CommentDbClass)
+
+    // 맨 위에 것 하나만 가져오기 - 쓸일이 있을지 의문...
+    @Query("Select * from commentDb Limit 1")
+    fun getComment(): CommentDbClass
+
+    // 해당 레시피 아이디에 맞는 레코드 하나를 가져오기
+    @Query("Select * from commentDb where commentId like :commentId LIMIT 1")
+    fun getCommentById(commentId: Int): CommentDbClass
+
+    // 사용 안함.
+    @Delete
+    suspend fun deleteComment(commentDbClass: CommentDbClass)
+
+    // 전체 코멘트 삭제에 사용할 것.
+    @Query("DELETE FROM commentDb")
+    suspend fun deleteAllComments()
+}
--- app/src/main/java/com/example/umc_zipdabang/src/main/roomDb/TokenDatabase.kt
+++ app/src/main/java/com/example/umc_zipdabang/src/main/roomDb/TokenDatabase.kt
@@ -8,13 +8,45 @@ import androidx.room.Room
 import androidx.room.RoomDatabase
 import java.time.chrono.HijrahChronology.INSTANCE
 
-@Database(entities = [Token::class], version=1)
+//@Database(entities = [Token::class], version=1)
+//abstract class TokenDatabase: RoomDatabase() {
+//    abstract fun tokenDao(): TokenDao
+//
+//    companion object {
+//        @Volatile
+//        private var INSTANCE: TokenDatabase? = null
+//
+//        fun getTokenDatabase(context: Context): TokenDatabase {
+//            val tempInstance = INSTANCE
+//            if (tempInstance != null) {
+//                return tempInstance
+//            }
+//            synchronized(this) {
+//                val instance = Room.databaseBuilder(
+//                    context.applicationContext, TokenDatabase::class.java, "tokenDb"
+//                ).build()
+//                INSTANCE = instance
+//                return instance
+//            }
+//        }
+//    }
+//}
+
+@Database(entities = [Token::class, RecipeDbClass::class, CommentDbClass::class], version=2, exportSchema = true)
 abstract class TokenDatabase: RoomDatabase() {
     abstract fun tokenDao(): TokenDao
+    abstract fun recipeDao(): RecipeDao
+    abstract fun commentDao(): CommentDao
 
     companion object {
         @Volatile
         private var INSTANCE: TokenDatabase? = null
+
+//        var MIGRATION_1_2: Migration = object: Migration(1, 2) {
+//            override fun migrate(database: SupportSQLiteDatabase) {
+//                database.execSQL("CREATE TABLE IF NOT EXISTS `RecipeDbClass` (`recipeId` INT NOT NULL, PRIMARY KEY(`recipeId`))")
+//            }
+//        }
 
         fun getTokenDatabase(context: Context): TokenDatabase {
             val tempInstance = INSTANCE
@@ -23,7 +55,7 @@ abstract class TokenDatabase: RoomDatabase() {
             }
             synchronized(this) {
                 val instance = Room.databaseBuilder(
-                    context.applicationContext, TokenDatabase::class.java, "tokenDb"
+                    context.applicationContext, TokenDatabase::class.java, "tokenDb2"
                 ).build()
                 INSTANCE = instance
                 return instance
