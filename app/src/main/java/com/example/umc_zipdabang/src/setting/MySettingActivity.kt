c2a34b731d6d5754d4ffcd2ecddbef5946f03ffa
HahyunKang <ganghahyeon06@gmail.com>
2023-01-24 21:40:34 +0900

    레이아웃 수정

---
+++ app/src/main/java/com/example/umc_zipdabang/src/setting/MySettingActivity.kt
@@ -0,0 +1,19 @@
+package com.example.umc_zipdabang.src.setting
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.umc_zipdabang.databinding.ActivityMySettingBinding
+
+class MySettingActivity :AppCompatActivity(){
+    private lateinit var viewBinding: ActivityMySettingBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        viewBinding = ActivityMySettingBinding.inflate(layoutInflater)
+        super.onCreate(savedInstanceState)
+        setContentView(viewBinding.root)
+
+        viewBinding.myBackbtn.setOnClickListener {
+            finish()
+        }
+    }
+}
