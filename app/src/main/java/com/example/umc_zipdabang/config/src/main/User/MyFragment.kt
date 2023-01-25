a6aa8e09e398c888c627a699385ea185e8409dc2
HahyunKang <ganghahyeon06@gmail.com>
2023-01-25 10:29:47 +0900

    수정

--- app/src/main/java/com/example/umc_zipdabang/config/src/main/User/MyFragment.kt
+++ app/src/main/java/com/example/umc_zipdabang/config/src/main/User/MyFragment.kt
@@ -7,7 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.umc_zipdabang.databinding.FragmentUserBinding
 
-class UserFragment: Fragment() {
+class MyFragment: Fragment() {
 
     private lateinit var  viewBinding: FragmentUserBinding
 
