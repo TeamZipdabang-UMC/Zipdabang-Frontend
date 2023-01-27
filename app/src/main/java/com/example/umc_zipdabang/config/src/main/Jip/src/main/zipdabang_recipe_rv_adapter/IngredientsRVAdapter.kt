125b155b50a08c595951f96228b66582a878ed25
Kim Gimun <kmkim7575@gmail.com>
2023-01-27 17:34:34 +0900

    [FEAT] 레시피 클릭 시 검색 기능 추가

--- app/src/main/java/com/example/umc_zipdabang/config/src/main/Jip/src/main/zipdabang_recipe_rv_adapter/IngredientsRVAdapter.kt
+++ app/src/main/java/com/example/umc_zipdabang/config/src/main/Jip/src/main/zipdabang_recipe_rv_adapter/IngredientsRVAdapter.kt
@@ -1,11 +1,21 @@
 package com.example.umc_zipdabang.config.src.main.Jip.src.main.zipdabang_recipe_rv_adapter
 
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.view.LayoutInflater
+import android.view.View
+import android.view.View.OnClickListener
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.example.umc_zipdabang.config.src.main.Jip.src.main.zipdabang_recipe_activities_fragments.IngredientDetail
+import com.example.umc_zipdabang.config.src.main.Jip.src.main.zipdabang_recipe_comment.ZipdabangRecipeDetailActivity
 import com.example.umc_zipdabang.config.src.main.Jip.src.main.zipdabang_recipe_data_class.Ingredient
 import com.example.umc_zipdabang.databinding.ItemIngredientsBinding
+import kotlinx.android.synthetic.main.item_ingredients.view.*
+import kotlinx.coroutines.withContext
 
 
 class IngredientsRVAdapter(private val ingredientsList: ArrayList<IngredientDetail>): RecyclerView.Adapter<IngredientsRVAdapter.IngredientsViewHolder>() {
@@ -17,7 +27,12 @@ class IngredientsRVAdapter(private val ingredientsList: ArrayList<IngredientDeta
             viewBinding.tvIngredientAmount.text = ingredient.quantity
             viewBinding.ivSearch.setOnClickListener {
 
+                val shoppingIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://search.shopping.naver.com/search/all?query=${ingredient.name}"))
+                itemView.context.startActivity(shoppingIntent)
             }
+
+
+
         }
     }
 
@@ -32,6 +47,11 @@ class IngredientsRVAdapter(private val ingredientsList: ArrayList<IngredientDeta
 
     override fun onBindViewHolder(holder: IngredientsViewHolder, position: Int) {
         holder.bind(ingredientsList[position])
+
+
+
+
+
     }
 
     override fun getItemCount(): Int = ingredientsList.size
